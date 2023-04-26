fe6b3d0981f6122f2366a13911c901757e3a27d5
root <root@DESKTOP-VETUQN7.localdomain>
2023-04-26 19:33:52 -0400

    add test and update scraper

--- src/scrape_urls.py
+++ src/scrape_urls.py
@@ -25,28 +25,56 @@ month_day_dictionary = {1: 31, 2: 28, 3: 30, 4: 30, 5: 31, 6: 30, 7: 31,
 html_url_1 = 'https://www.basketball-reference.com/boxscores/?month=7&day=13&year=2021'
 page1 = requests.get(html_url_1)
 soup1 = BeautifulSoup(page1.text, 'html.parser')
-print(soup1.find_all("strong")[1].getText())
+
+def get_url_date(year, month, day, urls):
+    html_url = "https://www.basketball-reference.com/boxscores" \
+               "/?month=" + str(month) + "&day=" + str(day) \
+               + "&year=" + str(year)
+    print(html_url)
+    page = requests.get(html_url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    print(soup.find_all("strong")[1].getText())
+    if "No games played on this date" != soup.find_all("strong")[
+        1].getText():
+
+        for line in soup.find_all("td", {"class": "right gamelink"}):
+            urls.append(
+                'https://www.basketball-reference.com/boxscores/pbp/'
+                + line.a.get('href').split('/')[-1])
+    elif "Pourly Formed Request (400 error)" == soup.find_all("strong")[1].getText():
+        print(soup.get_text)
 
 def get_urls(start_year, start_month, end_year, end_month):
-    number_of_years = end_year - start_year
+
     urls = []
-    for month_add in range(number_of_years * 12):
-        curr_year = month_add // 12 + start_year
-        curr_month = (month_add + start_month) % 12
-        if curr_year == end_year and curr_month == end_month:
-            return urls
-        for day in range(1, month_day_dictionary[curr_month] + 1):
-            html_url = "https://www.basketball-reference.com/boxscores" \
-                       "/?month="+ str(curr_month) + "&day="+ str(day) \
-                       + "&year=" + str(curr_year)
+    current_year = start_year
+    current_month = start_month
+    while current_year <= end_year:
 
-            page = requests.get(html_url)
-            soup = BeautifulSoup(page.text, 'html.parser')
-            if "No games played on this date" != soup.find_all("strong")[1].getText():
+        while current_month <= 12:
 
-                for line in soup.find_all("td", {"class": "right gamelink"}):
-                    urls.append(
-                        'https://www.basketball-reference.com/boxscores/pbp/'
-                        + line.a.get('href').split('/')[-1])
+            print(current_year, current_month)
+            for day in range(1, month_day_dictionary[current_month] + 1):
+                print(current_year, current_month, day)
+                html_url = "https://www.basketball-reference.com/boxscores" \
+                           "/?month="+ str(current_month) + "&day="+ str(day) \
+                           + "&year=" + str(current_year)
+                print(html_url)
+                page = requests.get(html_url)
+                soup = BeautifulSoup(page.text, 'html.parser')
+                print(soup.find_all("strong")[1].getText())
+                if "No games played on this date" != soup.find_all("strong")[1].getText():
+
+                    for line in soup.find_all("td", {"class": "right gamelink"}):
+                        urls.append(
+                            'https://www.basketball-reference.com/boxscores/pbp/'
+                            + line.a.get('href').split('/')[-1])
+
+            current_month += 1
+            if current_year == end_year and current_month == end_month + 1:
+                break
+        current_month = 1
+        current_year += 1
 
     return urls
+# a = get_urls(2022,11,2022,11)
---
+++ testing/2022_2023_season_game_urls.py
@@ -0,0 +1,120 @@
+oct = ['https://www.basketball-reference.com/boxscores/pbp/202210180BOS'
+       '.html', 'https://www.basketball-reference.com/boxscores/pbp'
+                '/202210180GSW.html',
+       'https://www.basketball-reference.com/boxscores/pbp/202210190ATL'
+       '.html', 'https://www.basketball-reference.com/boxscores/pbp'
+                '/202210190BRK.html',
+       'https://www.basketball-reference.com/boxscores/pbp/202210190DET'
+       '.html', 'https://www.basketball-reference.com/boxscores/pbp'
+                '/202210190IND.html',
+       'https://www.basketball-reference.com/boxscores/pbp/202210190MEM'
+       '.html', 'https://www.basketball-reference.com/boxscores/pbp'
+                '/202210190MIA.html',
+       'https://www.basketball-reference.com/boxscores/pbp/202210190MIN'
+       '.html', 'https://www.basketball-reference.com/boxscores/pbp'
+                '/202210190PHO.html',
+       'https://www.basketball-reference.com/boxscores/pbp/202210190SAC'
+       '.html', 'https://www.basketball-reference.com/boxscores/pbp'
+                '/202210190SAS.html',
+       'https://www.basketball-reference.com/boxscores/pbp/202210190TOR'
+       '.html', 'https://www.basketball-reference.com/boxscores/pbp'
+                '/202210190UTA.html',
+       'https://www.basketball-reference.com/boxscores/pbp/202210200LAL'
+       '.html', 'https://www.basketball-reference.com/boxscores/pbp'
+                '/202210200PHI.html',
+       'https://www.basketball-reference.com/boxscores/pbp/202210210ATL'
+       '.html', 'https://www.basketball-reference.com/boxscores/pbp'
+                '/202210210BRK.html',
+       'https://www.basketball-reference.com/boxscores/pbp/202210210CHO'
+       '.html', 'https://www.basketball-reference.com/boxscores/pbp'
+                '/202210210GSW.html',
+       'https://www.basketball-reference.com/boxscores/pbp/202210210HOU'
+       '.html', 'https://www.basketball-reference.com/boxscores/pbp'
+                '/202210210IND.html',
+       'https://www.basketball-reference.com/boxscores/pbp/202210210MIA'
+       '.html', 'https://www.basketball-reference.com/boxscores/pbp'
+                '/202210210MIN.html',
+       'https://www.basketball-reference.com/boxscores/pbp/202210210NYK'
+       '.html', 'https://www.basketball-reference.com/boxscores/pbp'
+                '/202210210POR.html',
+       'https://www.basketball-reference.com/boxscores/pbp/202210210WAS'
+       '.html', 'https://www.basketball-reference.com/boxscores/pbp'
+                '/202210220CHI.html',
+       'https://www.basketball-reference.com/boxscores/pbp/202210220DAL'
+       '.html', 'https://www.basketball-reference.com/boxscores/pbp'
+                '/202210220DEN.html',
+       'https://www.basketball-reference.com/boxscores/pbp/202210220IND'
+       '.html', 'https://www.basketball-reference.com/boxscores/pbp'
+                '/202210220MIA.html',
+       'https://www.basketball-reference.com/boxscores/pbp/202210220MIL'
+       '.html', 'https://www.basketball-reference.com/boxscores/pbp'
+                '/202210220ORL.html',
+       'https://www.basketball-reference.com/boxscores/pbp/202210220PHI'
+       '.html', 'https://www.basketball-reference.com/boxscores/pbp'
+                '/202210220SAC.html',
+       'https://www.basketball-reference.com/boxscores/pbp/202210230ATL'
+       '.html', 'https://www.basketball-reference.com/boxscores/pbp'
+                '/202210230CLE.html',
+       'https://www.basketball-reference.com/boxscores/pbp/202210230GSW'
+       '.html', 'https://www.basketball-reference.com/boxscores/pbp'
+                '/202210230LAC.html',
+       'https://www.basketball-reference.com/boxscores/pbp/202210230LAL'
+       '.html', 'https://www.basketball-reference.com/boxscores/pbp'
+                '/202210230NOP.html',
+       'https://www.basketball-reference.com/boxscores/pbp/202210230OKC'
+       '.html', 'https://www.basketball-reference.com/boxscores/pbp'
+                '/202210240CHI.html',
+       'https://www.basketball-reference.com/boxscores/pbp/202210240HOU'
+       '.html', 'https://www.basketball-reference.com/boxscores/pbp'
+                '/202210240MEM.html',
+       'https://www.basketball-reference.com/boxscores/pbp/202210240MIA'
+       '.html', 'https://www.basketball-reference.com/boxscores/pbp'
+                '/202210240MIN.html',
+       'https://www.basketball-reference.com/boxscores/pbp/202210240NYK'
+       '.html', 'https://www.basketball-reference.com/boxscores/pbp'
+                '/202210240PHI.html',
+       'https://www.basketball-reference.com/boxscores/pbp/202210240POR'
+       '.html', 'https://www.basketball-reference.com/boxscores/pbp'
+                '/202210250NOP.html',
+       'https://www.basketball-reference.com/boxscores/pbp/202210250OKC'
+       '.html', 'https://www.basketball-reference.com/boxscores/pbp'
+                '/202210250PHO.html',
+       'https://www.basketball-reference.com/boxscores/pbp/202210250WAS'
+       '.html', 'https://www.basketball-reference.com/boxscores/pbp'
+                '/202210260CHI.html',
+       'https://www.basketball-reference.com/boxscores/pbp/202210260CLE'
+       '.html', 'https://www.basketball-reference.com/boxscores/pbp'
+                '/202210260DEN.html',
+       'https://www.basketball-reference.com/boxscores/pbp/202210260DET'
+       '.html', 'https://www.basketball-reference.com/boxscores/pbp'
+                '/202210260MIL.html',
+       'https://www.basketball-reference.com/boxscores/pbp/202210260MIN'
+       '.html', 'https://www.basketball-reference.com/boxscores/pbp'
+                '/202210260NYK.html',
+       'https://www.basketball-reference.com/boxscores/pbp/202210260POR'
+       '.html', 'https://www.basketball-reference.com/boxscores/pbp'
+                '/202210260TOR.html',
+       'https://www.basketball-reference.com/boxscores/pbp/202210260UTA'
+       '.html', 'https://www.basketball-reference.com/boxscores/pbp'
+                '/202210270BRK.html',
+       'https://www.basketball-reference.com/boxscores/pbp/202210270GSW'
+       '.html', 'https://www.basketball-reference.com/boxscores/pbp'
+                '/202210270OKC.html',
+       'https://www.basketball-reference.com/boxscores/pbp/202210270SAC'
+       '.html', 'https://www.basketball-reference.com/boxscores/pbp'
+                '/202210280BOS.html',
+       'https://www.basketball-reference.com/boxscores/pbp/202210280DEN'
+       '.html', 'https://www.basketball-reference.com/boxscores/pbp'
+                '/202210280DET.html',
+       'https://www.basketball-reference.com/boxscores/pbp/202210280MIL'
+       '.html', 'https://www.basketball-reference.com/boxscores/pbp'
+                '/202210280MIN.html',
+       'https://www.basketball-reference.com/boxscores/pbp/202210280ORL'
+       '.html', 'https://www.basketball-reference.com/boxscores/pbp'
+                '/202210280PHO.html',
+       'https://www.basketball-reference.com/boxscores/pbp/202210280POR'
+       '.html', 'https://www.basketball-reference.com/boxscores/pbp'
+                '/202210280SAS.html',
+       'https://www.basketball-reference.com/boxscores/pbp/202210280TOR'
+       '.html', 'https://www.basketball-reference.com/boxscores/pbp'
+                '/202210280WAS.html']
